5ae3173a8c14327c14288cef8cd2b819cf3666c8
Vishal kumar <official.vishalkrrao@gmail.com>
2026-07-24 19:20:37 +0530

    Fix file casing for app.routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router"
-import { router } from "./app.routes"
+import { router } from "./app.routes.jsx"
 import { AuthProvider } from "./features/auth/auth.context.jsx"
 import { InterviewProvider } from "./features/Interview/interview.context.jsx"
 
